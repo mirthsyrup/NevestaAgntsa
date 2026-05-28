dd74bd95dfb9f157b925145f8fb787fd753ee3f2
Code Auditor <auditor@example.com>
2026-05-28 17:03:03 +0300

    Configure GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mirthsyrup.github.io',
+  base: '/NevestaAgntsa',
   integrations: [tailwind({
     // Отключаем базовые стили Tailwind в пользу кастомного импорта в global.css
     applyBaseStyles: false, 
